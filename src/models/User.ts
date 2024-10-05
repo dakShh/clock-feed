e59b23c1ba3986a4ea91585f22998cbc602a9a74
dakShh <daksh.dk207@gmail.com>
2024-10-05 00:53:05 -0400

    defining zod schemas for validation

--- src/models/User.ts
+++ src/models/User.ts
@@ -26,41 +26,44 @@ export interface User extends Document {
     message: Message[];
 }
 
-const UserSchema: Schema<User> = new Schema({
-    username: {
-        type: String,
-        required: [true, 'Username is required'],
-        trim: true,
-        unique: true,
+const UserSchema: Schema<User> = new Schema(
+    {
+        username: {
+            type: String,
+            required: [true, 'Username is required'],
+            trim: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: [true, 'Email is required'],
+            unique: true,
+            match: [/.+\@.+\..+/, 'Please use a valid email address'],
+        },
+        password: {
+            type: String,
+            required: [true, 'Password is required'],
+        },
+        verifyCode: {
+            type: String,
+            required: [true, 'Verify code is required'],
+        },
+        isVerified: {
+            type: Boolean,
+            default: false,
+        },
+        verifyCodeExpiry: {
+            type: Date,
+            required: [true, 'Verify code expiry is required'],
+        },
+        isAcceptingMessage: {
+            type: Boolean,
+            default: true,
+        },
+        message: [MessageSchema],
     },
-    email: {
-        type: String,
-        required: [true, 'Email is required'],
-        unique: true,
-        match: [/.+\@.+\..+/, 'Please use a valid email address'],
-    },
-    password: {
-        type: String,
-        required: [true, 'Password is required'],
-    },
-    verifyCode: {
-        type: String,
-        required: [true, 'Verify code is required'],
-    },
-    isVerified: {
-        type: Boolean,
-        default: false,
-    },
-    verifyCodeExpiry: {
-        type: Date,
-        required: [true, 'Verify code expiry is required'],
-    },
-    isAcceptingMessage: {
-        type: Boolean,
-        default: true,
-    },
-    message: [MessageSchema],
-});
+    { timestamps: true }
+);
 
 const UserModel: Model<User> = mongoose.models?.User || mongoose.model('User', UserSchema);
 
